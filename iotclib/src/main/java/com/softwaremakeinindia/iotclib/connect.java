d9b671f3b0f1b1a0b3684294fa5282ef0cabdd7d
VISHAL PANDEY <contact@vishalpandey.xyz>
2020-06-21 21:42:32 +0530

    First Commit

---
+++ iotclib/src/main/java/com/softwaremakeinindia/iotclib/connect.java
@@ -0,0 +1,10 @@
+package com.softwaremakeinindia.iotclib;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class connect {
+    public static void s(Context c, String message){
+        Toast.makeText(c, message, Toast.LENGTH_SHORT).show();
+    }
+}
